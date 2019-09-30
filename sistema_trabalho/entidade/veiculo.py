75b40f00dfa8a66b38f706092d7144d798db2588
Gustavocoelhocosta <gustavodiagramador@gmail.com>
2019-09-30 20:03:51 -0300

    criação de arquivos

---
+++ sistema_trabalho/entidade/veiculo.py
@@ -0,0 +1,2 @@
+class Veiculo():
+    def __init__(self, placa: str, modelo: str, marca: str, ano: int,  quilometragem_atual: int):
---
+++ sistema_trabalho/limite/telaAbstract.py
@@ -0,0 +1,5 @@
+from abc import ABC, abstractclassmethod
+
+
+class TelaAbstract(ABC):
+    pass
